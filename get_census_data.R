3c439626e8b7748412379423c7801dd7b6af535a
KarlGStahlfeld <47041363+KarlGStahlfeld@users.noreply.github.com>
2019-04-11 00:18:38 -0500

    Update get_census_data.R

--- get_census_data.R
+++ get_census_data.R
@@ -14,7 +14,7 @@ census_api_key("f4ea45dde45d543afab05aa7ce86b36d5992a42f")
 #                   state = "OR",
 #                  variables = c(hhincome = "B19013_001", 
 #                               medage = "B01002_001"), 
-#                output = "wide",                    year='2017')
+#                output = "wide",                    year=2017)
 #head(or_wide)
 # Create a scatterplot
 #plot(or_wide$hhincomeE, or_wide$medageE)
@@ -51,20 +51,21 @@ head(test)
 study_county_fips <- readRDS('CleanData/study_county_fips.rds')
 
 pc.Inc.15<-data.table(get_acs(geography = "tract",
-                   state = "VA",
-                   county = study_county_fips,
-                   variables = "DP03_0088E",
-                   output = "wide",                    year='2017',                    year='2017') 
-                   year='2015'))
+                              state = "VA",
+                              county = study_county_fips,
+                              variables = "DP03_0088E",
+                              output = "wide",                    
+                              year=2017)) 
+                      
 
 
 
 pc.Inc.17<-as.data.table(get_acs(geography = "tract",
-                   state = "VA",
-                   county = study_county_fips,
-                   variables = "DP03_0088E",
-                   output = "wide",                    year='2017',                    year='2017')
-                   year='2017'))
+                                 state = "VA",
+                                 county = study_county_fips,
+                                 variables = "DP03_0088E",
+                                 output = "wide",
+                                  year=2017))
 
 head(pc.Inc.17)
 view(pc.Inc.17)
@@ -73,10 +74,11 @@ med.Inc.17<-get_acs(geography = "tract",
                     state = "VA",
                     county = study_county_fips,
                     variables = "DP03_0086E",
-                    output = "wide",                    year='2017')
+                    output = "wide",                    year=2017)
 
 head(med.Inc.17)
 view(med.Inc.17)
+
 
 
 #Total Number of families?
@@ -84,7 +86,7 @@ view(med.Inc.17)
 #                        state = "VA",
 #                        county = study_county_fips,
 #                        variables = "DP03_0075E",
-#                        output = "wide",                    year='2017')
+#                        output = "wide",                    year=2017)
 ##roughly 5x as much as tot.race.17 with roughly half a dozen outliers?
 
 #Total commuting to work
@@ -93,61 +95,75 @@ tot.com.17<-get_acs(geography = "tract",
                     state = "VA",
                     county = study_county_fips,
                     variables = "DP03_0018E",
-                    output = "wide",                    year='2017')
+                    output = "wide",
+                    year=2017)
 
 carA.com.17<-get_acs(geography = "tract",
                      state = "VA",
                      county = study_county_fips,
                      variables = "DP03_0019E",
-                     output = "wide",                    year='2017')
+                     output = "wide",                    year=2017)
 carP.com.17<-get_acs(geography = "tract",
                      state = "VA",
                      county = study_county_fips,
                      variables = "DP03_0020E",
-                     output = "wide",                    year='2017')
+                     output = "wide",                    year=2017)
 
 
 pub.com.17<-get_acs(geography = "tract",
                     state = "VA",
                     county =study_county_fips,
                     variables = "DP03_0021E",
-                    output = "wide",                    year='2017')
+                    output = "wide",                    year=2017)
 
 walk.com.17<-get_acs(geography = "tract",
                      state = "VA",
                      county = study_county_fips,
                      variables = "DP03_0022E",
-                     output = "wide",                    year='2017')
+                     output = "wide",                    year=2017)
 
 meantime.com.17<-get_acs(geography = "tract",
                          state = "VA",
                          county = study_county_fips,
                          variables = "DP03_0025E",
-                         output = "wide",                    year='2017') #in minutes
+                         output = "wide",                    year=2017) #in minutes
 
 
 tot.race.17<-get_acs(geography = "tract",
                      state = "VA",
                      county = study_county_fips,
                      variables = "DP05_0033E",
-                     output = "wide",                    year='2017')
+                     output = "wide",                    
+                     year=2017)
 
 tot1.race.17<-get_acs(geography = "tract",
                       state = "VA",
                       county = study_county_fips,
                       variables = "DP05_0034E",
-                      output = "wide",                    year='2017') #difference between 0034 and 0036?
+                      output = "wide",                    year=2017) #difference between 0034 and 0036?
 white.race.17<-get_acs(geography = "tract",
                        state = "VA",
                        county = study_county_fips,
                        variables = "DP05_0037E",
-                       output = "wide",                    year='2017')
+                       output = "wide",                    year=2017)
 
 black.race.17<-get_acs(geography = "tract",
                        state = "VA",
                        county = study_county_fips,
                        variables = "DP05_0038E",
-                       output = "wide",                    year='2017')
+                       output = "wide",                    year=2017)
+
+asian.race.17<-get_acs(geography = "tract",
+                       state = "VA",
+                       county = study_county_fips,
+                       variables = "DP05_0044E",
+                       output = "wide",                    year=2017)
+
+hisp.race.17<-get_acs(geography = "tract",
+                      state = "VA",
+                      county = study_county_fips,
+                      variables = "DP05_0070E",
+                      output = "wide",                    year=2017)
 
 #Other races are after DP05_0038E if we want to add them too.
 
@@ -163,12 +179,14 @@ merge.com.17.final<-merge(tot.com.17,merge.com.17.time)
 
 merge.race.17.wb<-merge(white.race.17, black.race.17)
 merge.race.17.T1<-merge(tot.race.17,tot1.race.17)
+merge.race.17.ha<-merge(asian.race.17,hisp.race.17)
 
+merge.race.17.all<-merge(merge.race.17.wb, merge.race.17.ha)
 merge.race.17.Final<-merge(merge.race.17.T1,merge.race.17.wb)
 
 merge.inc.race.17<-merge(merge.inc.17,merge.race.17.Final)
 merge.allthree.17<-merge(merge.inc.race.17,merge.com.17.final)
-
+#####
 ##### stuff with 10 year census, shouldn't need
 #Census apparently doesn't track income, use it for race? https://api.census.gov/data/2010/dec/sf1/variables.html
 #This is for blacks only, see webiste to get other races
@@ -176,6 +194,18 @@ test3<-get_decennial(geography = "block",
                      state = "VA",
                      county = study_county_fips,
                      variables = "H006003", 
-                     output = "wide",                    year='2017')
+                     output = "wide",                    year=2017)
 head(test3)
 view(test3)
+#####
+#updated names, removed the margin of error, did best to try to match what I have with the code from functions.segregation.R
+
+#I only included black and white for race. Go back and add other races?
+#I only have stuff for fist three functions, nothing after race, so do we need stuff for tables etc.?
+
+funCensus.income<-med.Inc.17[,-4]
+
+funCensus.race<-merge.race.17.all[,c(1,2,3,7,9,11)]
+
+funCensus.commute<-merge.com.17.final[,c(1,2,3,5,7,9,11)]
+
